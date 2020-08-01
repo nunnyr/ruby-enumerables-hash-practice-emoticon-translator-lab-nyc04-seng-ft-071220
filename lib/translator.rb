6318af5e2a2ad489c28d42042bade75cc054859e
Nunnyr <nunnyreyes@gmail.com>
2020-08-01 19:08:41 -0400

    working on syntax

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,34 +4,42 @@ require "pry"
 
 def load_library(file)
   # code goes here
-  emoticons = YAML.load_file("./lib/emoticons.yml")
+
+  emoticons = YAML.load_file(file)
   new_hash = {}
   
-  
-  #I am creating a new hash where each key is the name of the emoticon
-  #the hash I am really iterating over is the emoticons
- emoticons.each do |key, value|
-  #binding.pry
-  #now I have my key which is the name
-  #each Key has 2 values: english and my japenese emoticon
-    new_hash[key] = {}
-    
-    #below is the way to write a new key and set it equal to its value
-    #get that through your head
-    #when you write the key you have to put it in symbol notation
-    #the reason why key is not in symbol notation is because we are pulling it from the each iteration
-    #if i just have it set to value and not value at index 0 then i am putting all of the 
-    #values in it. i just want the english one
-    new_hash[key][:english] = value[0]
-    new_hash[key][:japanese] = value[1]
+  emoticons.each do |name_of_emoji, language|
+                      #key:angel     #value -somethingEnglish - something Japanese
 
-    # binding.pry
-    end
+  #new_hash[name_of_emoji] = {}
+ 
+  new_hash[name_of_emoji][:english] = language[0]
+  
+  new_hash[name_of_emoji][:japanese] = language[1]
+
+
+
+  end
   new_hash
 end
 
-# load_library(file)
+#I am creating a new hash where each key is the name of the emoticon. aka "angel"
+  #the hash I am really iterating over is the emoticons (which are the values)
+  #binding.pry
+  #now I have my key which is the name
+  #each Key has 2 values: english and my japenese emoticon
+    
+    
+    #binding.pry
+    #below is the way to write a new key and set it equal to its value
+    #when you write the key you have to put it in symbol notation
+    
+    #if i just have it set to value and not value at index 0 then i am putting all of the 
+    #values in it. i just want the english one
+    
 
+# load_library(file)
+#the reason why key is not in symbol notation is because we are pulling it from the each iteration
 def get_japanese_emoticon(file, emoji)
   # code goes here
   library = load_library(file)
